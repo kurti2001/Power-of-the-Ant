de78bf922a79fa1437dbe4a30adbc10125e533b3
october1101 <63201898+october1101@users.noreply.github.com>
2023-06-17 00:21:02 +0200

    Part 1 of the example [needs improvements]

--- Power of the Ant/Program.cs
+++ Power of the Ant/Program.cs
@@ -1,12 +1,85 @@
 ﻿using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
 
-namespace Power_of_the_Ant
+class Player
 {
-    class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
+        Console.WriteLine("Give the X position of the colony, the Y position of the colony, the ANT's X starting position and the ANT's Y starting position\n\n");
+
+        int colonyX = Convert.ToInt32(Console.ReadLine());
+
+        int colonyY = Convert.ToInt32(Console.ReadLine());// the Y position of the colony
+
+        int initialX = Convert.ToInt32(Console.ReadLine());// The Ant's starting X position
+
+        int initialY = Convert.ToInt32(Console.ReadLine());
+
+
+
+        int remainingTurns = int.Parse(Console.ReadLine()); // The remaining amount of turns the Ant can move. Do not remove this line.
+        bool Destination = true;
+        while (Destination = true)
         {
-            Console.WriteLine("Hello World!");
+            int movement = 1;
+
+            if (colonyX > initialX && colonyY > initialY)
+            {
+                Console.WriteLine("NE");
+                initialX = initialX + movement;
+                initialY = initialY + movement;
+
+            }
+            else if (colonyX < initialX && colonyY < initialY)
+            {
+                Console.WriteLine("SW");
+                initialY = initialY - movement;
+                initialX = initialX - movement;
+            }
+            else if (colonyX == initialX && colonyY > initialY)
+            {
+                Console.WriteLine("N");
+                initialY = initialY + movement;
+            }
+            else if (colonyX > initialX && colonyY == initialY)
+            {
+                Console.WriteLine("E");
+                initialX = initialX + movement;
+            }
+            else if (colonyX > initialX && colonyY < initialY)
+            {
+                Console.WriteLine("SE");
+                initialY = initialY - movement;
+                initialX = initialX + movement;
+            }
+            else if (colonyX == initialX && colonyY < initialY)
+            {
+                Console.WriteLine("S");
+                initialY = initialY - movement;
+            }
+            else if (colonyX < initialX && colonyY == initialY)
+            {
+                Console.WriteLine("W");
+                initialX = initialX - movement;
+            }
+            else if (colonyX < initialX && colonyY > initialY)
+            {
+                Console.WriteLine("NW");
+                initialY = initialY + movement;
+                initialX = initialX - movement;
+            }
+            else if (colonyX == initialX && colonyY == initialY)
+            {
+                Console.WriteLine("Ant arrived the destination");
+                Destination = false;
+                return;
+            }
+
         }
     }
 }
+
